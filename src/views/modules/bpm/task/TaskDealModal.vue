d9adce6109b73a948473318467ccd0ba991dc93f
Yoko <kuluseky@gmail.com>
2024-08-23 13:05:54 +0800

    feat: 基座-重构流程审批组件，彻底解决在自动渲染三种表单时kForm类型组件加载异常

--- src/views/modules/bpm/task/TaskDealModal.vue
+++ src/views/modules/bpm/task/TaskDealModal.vue
@@ -30,17 +30,16 @@
           <a-icon type="file-text"/>
           <span>业务信息</span>
         </span>
-        <a-spin :spinning="pageLoading">
-          <div class="component_div" v-if="!pageLoading">
+        <div class="component_div">
+          <template v-if="isComp">
             <!--如果是kform表单设计器类型的走设计器-->
             <!--:onlineTableId="formData.tableName"-->
             <desform-view
-              v-if="formData && formData.formType === FORM_TYPE_DESIGNFORM"
+              v-if="formData.dataId && formData.formType === FORM_TYPE_DESIGNFORM"
               class="desform-view"
               :mode="mode"
               :desformCode="formData.tableName"
               :dataId="formData.dataId"
-              :online-table-id="formData.cgformId"
               height="100vh"
               :innerDialog="true"
               @reload="handleReload"
@@ -49,14 +48,17 @@
               ref="realForm"
             />
             <!--online和编码类型的走组件-->
-            <dynamic-link v-if="[FORM_TYPE_ONLINE, FORM_TYPE_CODE].includes(formData.formType)" ref="realForm" :path="path" :formData="formData"></dynamic-link>
-
-            <template v-if="!isComp">
-              <!--旧版本url版本的表单设计器以iframe形式加载-->
-              <iframe :src="iframeUrl" frameborder="0" width="100%" :height="height" scrolling="auto"></iframe>
-            </template>
-          </div>
-        </a-spin>
+            <dynamic-link
+              v-if="formData.dataId && [FORM_TYPE_ONLINE, FORM_TYPE_CODE].includes(formData.formType)"
+              ref="realForm"
+              :path="path"
+              :formData="formData" />
+          </template>
+          <template v-else>
+            <!--旧版本url版本的表单设计器以iframe形式加载-->
+            <iframe :src="iframeUrl" frameborder="0" width="100%" :height="height" scrolling="auto"></iframe>
+          </template>
+        </div>
       </a-tab-pane>
 
       <a-tab-pane key="2">
@@ -77,14 +79,11 @@
 
     </a-tabs>
   </j-modal>
-
   <!--</a-modal>-->
 </template>
 
 <script>
 
-import { getAction } from '@/api/manage'
-import DesformView from '@/components/online/desform/DesformView'
 import { ACCESS_TOKEN } from '@/store/mutation-types'
 import { isURL } from '@/utils/validate'
 import Vue from 'vue'
@@ -104,24 +103,39 @@ export default {
     DynamicLink,
     TaskModule,
     ProcessModule,
-    DesformView
+    // 解决父组件 BindBpm 被OnlCgformAutoList组件内部引入，存在重复引用导致的组件无法注册问题
+    DesformView: () => import('@/components/online/desform/DesformView')
   },
-  // eslint-disable-next-line vue/require-prop-types
-  props: ['path', 'formData'],
+  props: {
+    path: {
+      type: String,
+      default: ''
+    },
+    formData: {
+      type: Object,
+      default: () => ({
+        taskDefKey: '',
+        formType: '',
+        tableName: '',
+        // 表单数据id
+        dataId: ''
+      })
+    }
+  },
   computed: {
     // kForm表单的展示形式
     mode() {
       return (this.formData && this.formData.disabled) ? 'detail' : 'edit'
     },
     // 是否为组件
-    isComp: function () {
+    isComp() {
       console.log('isComp组件名称：', this.path)
       var TOKEN = Vue.ls.get(ACCESS_TOKEN)
       var DOMAIN_URL = window._CONFIG['domianURL']
       var TASKID = this.formData.taskDefKey
       // eslint-disable-next-line no-eval
       var URL = (this.path || '').replace(/{{([^}}]+)?}}/g, (s1, s2) => eval(s2)) // URL支持{{ window.xxx }}占位符变量
-      console.log('isComp组件名称：', URL, this.path, this.formData)
+      console.log('isComp组件名称：', URL)
       // 判断是否是旧的设计器表单（以 {{DOMAIN_URL}}/desform/ 开头）
       if (isURL(URL)) {
         // eslint-disable-next-line vue/no-side-effects-in-computed-properties
@@ -153,18 +167,14 @@ export default {
       bgColor: 'rgba(0,0,0,0.6)',
       isOnline: false,
       url: {
-        getKFormMetaData: '/desform/queryByCode',
         getFormData: '/online/cgform/api/form/table_name'
       },
-      pageLoading: true,
-      cgformId: '',
       FORM_TYPE_ONLINE,
       FORM_TYPE_DESIGNFORM,
       FORM_TYPE_CODE
     }
   },
   created() {
-    this.pageLoading = true
   },
   methods: {
     /**
@@ -204,30 +214,7 @@ export default {
     },
     handleReload(data) {
       this.$emit('reload', { target: this })
-    },
-    /**
-     * kForm表单设计器加载表单数据
-     * @returns {Promise<unknown>}
-     */
-    loadFormJson() {
-      console.log('--TaskDealModal--loadFormJson--加载表单类型：', this.formData.formType, this.formData)
-      return new Promise((resolve) => {
-        if (!this.formData || this.formData.formType !== FORM_TYPE_DESIGNFORM) {
-          this.pageLoading = false
-          resolve()
-          return
-        }
-        // 加载kForm表单设计器
-        const params = { desformCode: this.formData.tableName }
-        getAction(this.url.getKFormMetaData, params).then(res => {
-          this.formDataJson = JSON.parse(res.result.desformDesignJson)
-          this.formData.cgformId = res.result.cgformId
-          this.pageLoading = false
-          console.log('--TaskDealModal--loadFormJson--加载kForm表单设计器：', res)
-          resolve()
-        })
-      })
-    },
+    }
     // /**
     //  * 旧版本表单设计器加载逻辑
     //  * @deprecated
@@ -249,22 +236,6 @@ export default {
     //     })
     //   })
     // },
-    init() {
-      if (this.formData && Object.keys(this.formData).length) {
-        this.pageLoading = true
-        this.$nextTick(async () => {
-          await this.loadFormJson()
-          // await this.loadFormData()
-        })
-      }
-    }
-  },
-  watch: {
-    // 初始化
-    'formData.dataId': {
-      handler: 'init',
-      immediate: true
-    }
   }
 }
 </script>
