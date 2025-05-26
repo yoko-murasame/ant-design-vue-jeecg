682398737937ee61028d46e4308ce3d104bec27e
Yoko <kuluseky@gmail.com>
2025-05-27 00:41:29 +0800

    chore: base-upgrade to NodeJS 18 + Pnpm

--- vue.config.js
+++ vue.config.js
@@ -103,13 +103,15 @@ module.exports = {
   css: {
     loaderOptions: {
       less: {
-        modifyVars: {
-          /* less 变量覆盖，用于自定义 ant design 主题 */
-          'primary-color': '#1890FF',
-          'link-color': '#1890FF',
-          'border-radius-base': '4px'
-        },
-        javascriptEnabled: true
+        lessOptions: {
+          modifyVars: {
+            /* less 变量覆盖，用于自定义 ant design 主题 */
+            'primary-color': '#1890FF',
+            'link-color': '#1890FF',
+            'border-radius-base': '4px'
+          },
+          javascriptEnabled: true
+        }
       }
     }
   },
